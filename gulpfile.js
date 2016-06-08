460d081a293f7b8f4af313204acf84b466d185a8
Jerome Martinez <jerome.e.martinez@gmail.com>
2016-06-08 13:08:29 -0500

    super gulp!

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,10 @@ var wrench = require('wrench');
  *  This will load all js or coffee files in the gulp directory
  *  in order to load all gulp tasks
  */
-wrench.readdirSyncRecursive('./gulp').filter(function(file) {
+wrench.readdirSyncRecursive('./super-gulp').filter(function(file) {
   return (/\.(js|coffee)$/i).test(file);
 }).map(function(file) {
-  require('./gulp/' + file);
+  require('./super-gulp/' + file);
 });
 
 
